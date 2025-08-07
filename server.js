22c3ca9a2f19bce23a1038cdd8c4f422890b6f81
Mateus Kronbauer Pitta <contato.mateuspitta@gmail.com>
2025-08-07 16:12:08 -0400

    novas 6

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 "use strict";
-
-// Carrega variáveis de ambiente
 if (process.env.RAILWAY_ENVIRONMENT) {
   process.env.NODE_ENV = process.env.NODE_ENV || "production";
 } else {
   require("dotenv").config();
 }
+/*
+|--------------------------------------------------------------------------
+| Http server
+|--------------------------------------------------------------------------
+|
+| This file bootstraps Adonisjs to start the HTTP server. You are free to
+| customize the process of booting the http server.
+|
+| """ Loading ace commands """
+|     At times you may want to load ace commands when starting the HTTP server.
+|     Same can be done by chaining `loadCommands()` method after
+|
+| """ Preloading files """
+|     Also you can preload files by calling `preLoad('path/to/file')` method.
+|     Make sure to pass a relative path from the project root.
+*/
 
-// Única instância do Ignitor
 const { Ignitor } = require("@adonisjs/ignitor");
 
 new Ignitor(require("@adonisjs/fold"))
